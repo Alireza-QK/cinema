8ae039d58f0f383be530f1c1b6112bfcaad29474
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-24 21:57:17 +0430

    added CinemaAdmin

--- src/ticketing/admin.py
+++ src/ticketing/admin.py
@@ -5,6 +5,10 @@ class MovieAdmin(admin.ModelAdmin):
     list_display = ('name', 'director', 'year', 'length', 'country', 'language', 'description')
 
 
+class CinemaAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'capacity', 'phone', 'address')
+
+
 admin.site.register(Movie, MovieAdmin)
-admin.site.register(Cinema)
+admin.site.register(Cinema, CinemaAdmin)
 admin.site.register(ShowTime)
