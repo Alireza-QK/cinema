6419f359e69bc142c38f582197a0dc9521b72001
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-24 15:39:30 +0430

    Make model ShowTime

--- src/ticketing/models.py
+++ src/ticketing/models.py
@@ -33,3 +33,37 @@ class Cinema(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ShowTime(models.Model):
+    """
+        Represents a movie in a cinema at a specfic time
+    """
+
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='showtimes')
+    cinema = models.ForeignKey(Cinema, on_delete=models.CASCADE, related_name='showtimes')
+    
+    start_time = models.DateTimeField(verbose_name='تاریخ اکران')
+    end_time = models.DateTimeField(verbose_name='تاریخ پایان اکران')
+    price = models.BigIntegerField(verbose_name='قیمت بلیت')
+    salabel_seats = models.IntegerField(verbose_name='صندلی های قابل فروش')
+    free_seats = models.IntegerField(verbose_name='صندلی های خالی')
+
+    SALE_NOT_STARTED = 1
+    SALE_OPEN = 2
+    TICKETS_SOLD = 3
+    SALE_CLOSED = 4
+    MOVIE_PLAYED = 5
+    SHOW_CANCELED = 6
+    status_choices = (
+        (SALE_NOT_STARTED, 'فروش آغاز نشده'),
+        (SALE_OPEN, 'در حال فروش بلیت'),
+        (TICKETS_SOLD, 'بلیت ها تمام شد'),
+        (SALE_CLOSED, 'فروش بلیت بسته شد'),
+        (MOVIE_PLAYED, 'فیلم پخش شد'),
+        (SHOW_CANCELED, 'سانس لغو شد')
+    )
+    status = models.IntegerField(verbose_name='وضعیت سانس', choices=status_choices)
+    
+    def __str__(self):
+        return '{} - {} - {}'.format(self.movie, self.cinema, self.start_time)
