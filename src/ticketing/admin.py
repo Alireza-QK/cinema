27bc546e7c225214335ad2e92293e87e42de84e5
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-24 21:55:42 +0430

    added MovieAdmin

--- src/ticketing/admin.py
+++ src/ticketing/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Movie, Cinema, ShowTime
 
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'director', 'year', 'length', 'country', 'language', 'description')
 
-admin.site.register(Movie)
+
+admin.site.register(Movie, MovieAdmin)
 admin.site.register(Cinema)
 admin.site.register(ShowTime)
