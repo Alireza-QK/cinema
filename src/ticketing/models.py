cd3114a831b25804866368ba201059815a38dc1d
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-24 11:58:11 +0430

    add model Movie

--- src/ticketing/models.py
+++ src/ticketing/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    """
+        Represent a move
+    """
+
+    name = models.CharField(max_length=254, verbose_name='نام فیلم')
+    director = models.CharField(max_length=64, verbose_name='کارگردان')
+    year = models.DateTimeField(verbose_name='تاریخ انتشار')
+    length = models.PositiveIntegerField(verbose_name='زمان فیلم')
+    country = models.CharField(max_length=64, verbose_name='کشور سازنده')
+    language = models.CharField(max_length=32, verbose_name='زبان فیلم')
+    description = models.TextField(verbose_name='توضیحات', blank=True)
+    cover = models.ImageField(verbose_name='تصویر فیلم', upload_to='movie/covers/')
+
+    def __str__(self):
+        return self.name
